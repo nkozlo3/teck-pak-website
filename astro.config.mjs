ce29bcc5534050780130980f81f895782e18c502
Aleks <leinkozlo7@gmail.com>
2026-03-27 17:38:40 -0500

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nkozlo3@github.io',
+  base: '/teck-pak-website',
   vite: {
     plugins: [tailwindcss()]
   }
