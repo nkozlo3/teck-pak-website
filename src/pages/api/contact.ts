b9c490907710932cee86e725464af5d114548ae7
nkozlo3 <leinkozlo7@gmail.com>
2026-03-28 18:25:24 -0500

    key safety

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,7 +1,9 @@
 import type { APIRoute } from "astro";
 import { Resend } from "resend";
 
-const resend = new Resend(import.meta.env.RESEND_API_KEY);
+const RESEND_API_KEY = process.env.RESEND_API_KEY;
+
+const resend = new Resend(RESEND_API_KEY);
 
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.json();
