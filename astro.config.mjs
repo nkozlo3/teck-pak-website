a40c86db42971afbc177b9436d70b5aabe672bda
nkozlo3 <leinkozlo7@gmail.com>
2026-03-28 18:12:46 -0500

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nkozlo3@github.io',
-  base: '/teck-pak-website',
+  output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
 });
